5aa673df0b1f2745dbdf8832173f57bacb52061f
P-Tanjim <moksedurtanjim27@gmail.com>
2026-03-05 01:55:15 +0600

    make third section of video. Add opening and closing animation to the video

--- script.js
+++ script.js
@@ -24,29 +24,56 @@ function navAnimation() {
         tl.timeScale(2).reverse();
     });
 }
-const rightElem = document.querySelectorAll(".right-elem");
 
-function page2Animation(){
+function page2Animation() {
     rightElem.forEach((elem) => {
-    const img = elem.querySelector("img");
-    elem.addEventListener("mouseenter", () => {
-        img.style.opacity = 1;
-        img.style.scale = 1;
-    });
-    elem.addEventListener("mouseleave", () => {
-        img.style.opacity = 0;
-        img.style.scale = 0;
-    });
-    elem.addEventListener("mousemove", (e) => {
-        if(e.ClintX < elem.getBoundingClientRect.x){ 
+        const img = elem.querySelector("img");
+        elem.addEventListener("mouseenter", () => {
+            img.style.opacity = 1;
+            img.style.scale = 1;
+        });
+        elem.addEventListener("mouseleave", () => {
             img.style.opacity = 0;
             img.style.scale = 0;
-        }
-        img.style.left = `${e.x -elem.getBoundingClientRect().x}px`;
-        img.style.top = `${e.y -elem.getBoundingClientRect().y}px`;
-        img.style.transform = 'translate(-50%, -50%)';
+        });
+        elem.addEventListener("mousemove", (e) => {
+            if (e.ClintX < elem.getBoundingClientRect.x) {
+                img.style.opacity = 0;
+                img.style.scale = 0;
+            }
+            img.style.left = `${e.x - elem.getBoundingClientRect().x}px`;
+            img.style.top = `${e.y - elem.getBoundingClientRect().y}px`;
+            img.style.transform = 'translate(-50%, -50%)';
+        })
     })
-})
 }
+
+function page3Animation() {
+    videoPlay.addEventListener("click", () => {
+        video.play();
+        gsap.to(video, {
+            transform: "scaleX(1) scaleY(1)",
+            top: 0,
+            opacity: 1,
+            borderRadius: 0
+        });
+        video.currentTime = 0
+    });
+    video.addEventListener("click", () => {
+        video.pause();
+        gsap.to(video, {
+            transform: "scaleX(0.7) scaleY(0)",
+            top: "35%",
+            opacity: 0,
+            borderRadius: "30px"
+        })
+    });
+}
+
+const rightElem = document.querySelectorAll(".right-elem");
+const videoPlay = document.querySelector(".video-play .icon");
+const video = document.querySelector("#page3 video");
+
 navAnimation();
 page2Animation();
+page3Animation();
